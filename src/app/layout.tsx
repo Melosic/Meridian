8be9a4b707d8e1ac72c65a633e3a265a944592f5
Melosic <3322944056@qq.com>
2026-03-02 16:19:42 +0800

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AppProvider } from '@/components/AppProvider';
+import { Analytics } from "@vercel/analytics/next"
 
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+        <body>
+        {children}
+        <Analytics />
+        </body>
+        </html>
+    )
+}
 export const metadata: Metadata = {
   title: "Meridian",
   description: "个人二手卖家轻量级利润记账应用",
